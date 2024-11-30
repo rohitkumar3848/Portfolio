1daeeba77c2dca04c04966c149389faf520f4403
Rohit Kumar <125601198+rohitkumar3848@users.noreply.github.com>
2024-11-30 13:28:34 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -635,7 +635,7 @@ export const projects = [
     id: "project-10",
     title: "Portfolio",
     github: "https://github.com/rohitkumar3848/Portfolio",
-    link: "https://67364341c0646f34815748c3--silly-kheer-077077.netlify.app/",
+    link: "https://portfolio-jqso.onrender.com",
     image: portfolio,
     content: "A personal portfolio website built with React and styled using Tailwind CSS, showcasing projects, skills, and experience in a clean, responsive design. This site provides a streamlined, professional layout that highlights accomplishments and technical expertise effectively.",
     stack: [
